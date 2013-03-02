42d2aa88a013815fa6903311dbd3b3e9c67dd1c2
George Ornbo <george@shapeshed.com>
2013-03-02 08:40:48 +0000

    js style

--- app.js
+++ app.js
@@ -3,11 +3,11 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var express = require('express'),
+  routes = require('./routes'),
+  user = require('./routes/user'),
+  http = require('http'),
+  path = require('path');
 
 var app = express();
 
