2f84d9daa77837972fae264cf19c5e4e1c824bc0
George Ornbo <george@shapeshed.com>
2016-10-05 10:53:51 +0100

    [edit] rename router file

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var routes = require('./routes/router');
+var routes = require('./routes/');
 
 var app = express();
 
