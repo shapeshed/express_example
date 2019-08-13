ebc9fa16deb7cfe1ff24c67145c81a03c0133d92
Oppija <oppija@tite.amiedu-ict.net>
2019-08-13 14:48:00 +0300

    Testi virhe

--- test/app.test.js
+++ test/app.test.js
@@ -12,7 +12,7 @@ describe('GET /', function(){
 
   it('should return a 200 status code', function (done){
     http.get({ host: '0.0.0.0', port: 3000 }, function(res) {
-      assert.deepEqual(res.statusCode, 200);
+      assert.deepEqual(res.statusCode, 300);
       done();
     }).on('error', function(e) {
       throw new Error(e);
